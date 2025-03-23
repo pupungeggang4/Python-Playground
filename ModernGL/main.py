a90ea4cf41d6664f79d855ca6fdfe46f24e9a360
pupungeggang2 <kyongkyong2892@gmail.com>
2025-03-23 17:35:06 +0900

    Added ModernGL Pygame Study

---
+++ ModernGL/main.py
@@ -0,0 +1,50 @@
+import math
+import os
+import sys
+
+import numpy as np
+import moderngl
+import pygame
+
+pygame.init()
+pygame.display.set_mode((800, 800), flags=pygame.OPENGL | pygame.DOUBLEBUF, vsync=True)
+
+class Scene:
+    def __init__(self):
+        self.ctx = moderngl.get_context()
+        self.program = self.ctx.program(
+            vertex_shader='''
+                #version 330
+                in vec2 in_vert;
+                void main() {
+                    gl_Position = vec4(in_vert, 0.0, 1.0);
+                }
+            ''',
+            fragment_shader='''
+                #version 330
+                out vec4 f_color;
+                void main() {
+                    f_color = vec4(1.0, 0.0, 0.0, 1.0);
+                }
+            ''',
+        )
+
+        self.vertices = np.array([-0.6, -0.6, 0.6, -0.6, 0.0, 0.6], dtype='f4')
+        self.vbo = self.ctx.buffer(self.vertices)
+        self.vao = self.ctx.simple_vertex_array(self.program, self.vbo, 'in_vert')
+
+    def render(self):
+        self.ctx.clear()
+        self.vao.render()
+
+scene = Scene()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    scene.render()
+
+    pygame.display.flip()
