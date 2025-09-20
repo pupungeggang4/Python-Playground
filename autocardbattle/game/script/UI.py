8a33d7f3c165859f46f28463a5943524ba96e455
pupungeggang2 <kyongkyong2892@gmail.com>
2025-09-20 22:17:03 +0900

    Start

---
+++ autocardbattle/game/script/UI.py
@@ -0,0 +1,13 @@
+class UI:
+    class Title:
+        text_title = []
+        button_start = []
+        text_start = []
+        button_erase = []
+        text_erase = []
+
+    class Ready:
+        pass
+
+    class Battle:
+        pass
