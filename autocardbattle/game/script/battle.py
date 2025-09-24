54faa11ba9ff0b7f395131405608973018df5944
pupungeggang2 <kyongkyong2892@gmail.com>
2025-09-25 03:49:55 +0900

    Temp

--- autocardbattle/game/script/battle.py
+++ autocardbattle/game/script/battle.py
@@ -1,2 +1,17 @@
 class Battle():
-    pass
+    def __init__(self):
+        self.field = [
+            None, None, None, None, None, None, None, None, None, None
+        ]
+        self.player = BattlePlayer()
+        self.enemy = BattlePlayer()
+        self.turn = 0
+        self.turn_who = 0
+
+class BattlePlayer():
+    def __init__(self):
+        self.crystal_num = 0
+        self.hand_crystal = []
+        self.deck_crystal = []
+        self.hand = []
+        self.deck = []
