1bbc53917756244c70f756b5991416d82564c4dc
pupungeggang2 <kyongkyong2892@gmail.com>
2025-11-08 00:37:14 +0900

    Start

--- physics2d/src/script/platform.py
+++ physics2d/src/script/platform.py
@@ -10,3 +10,9 @@ from script.render import *
 class Platform(Entity):
     def __init__(self):
         pass
+
+    def handle_tick(self, game):
+        pass
+
+    def render(self, game):
+        pass
